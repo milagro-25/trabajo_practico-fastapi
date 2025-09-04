623d2f6ba1b7d6ba69b31ad714ad4c46f57a8e82
milagro-25 <yayivedia@gmail.com>
2025-09-03 22:01:29 -0300

    fastAPI

---
+++ app/repositorios/repositorio_menu.py
@@ -0,0 +1,35 @@
+from modelos.modelo_menu import Menu, MenuCreate
+
+# Base de datos en memoria
+db_menus = []
+id_counter = 1
+
+def get_all_menus():
+    return db_menus
+
+def get_menu_by_id(menu_id: int):
+    return next((menu for menu in db_menus if menu.id == menu_id), "menu")
+
+def create_menu(menu: MenuCreate):
+    global id_counter
+    new_menu = Menu(id=id_counter, **menu.dict())
+    db_menus.append(new_menu)
+    id_counter += 1
+    return new_menu
+
+def update_menu(menu_id: int, menu_data: MenuCreate):
+    menu = get_menu_by_id(menu_id)
+    if menu:
+        menu.dia = menu_data.dia
+        menu.principal = menu_data.principal
+        menu.guarnicion = menu_data.guarnicion
+        menu.bebida = menu_data.bebida
+        menu.postre = menu_data.postre
+    return menu
+
+def delete_menu(menu_id: int):
+    global db_menus
+    menu = get_menu_by_id(menu_id)
+    if menu:
+        db_menus = [m for m in db_menus if m.id != menu_id]
+    return menu
---
+++ app/servicios/servicios_menu.py
@@ -0,0 +1,17 @@
+from modelos.modelo_menu import MenuCreate
+from repositorios.repositorio_menu import menu_repository
+
+def listar_menus():
+    return menu_repository.get_all_menus()
+
+def obtener_menu(menu_id: int):
+    return menu_repository.get_menu_by_id(menu_id)
+
+def crear_menu(menu: MenuCreate):
+    return menu_repository.create_menu(menu)
+
+def actualizar_menu(menu_id: int, menu: MenuCreate):
+    return menu_repository.update_menu(menu_id, menu)
+
+def eliminar_menu(menu_id: int):
+    return menu_repository.delete_menu(menu_id)
